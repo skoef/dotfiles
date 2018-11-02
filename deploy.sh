fb61d3c717a3300d49fe295798c3d0c987742f6f
Reinier Schoof <reinier@skoef.nl>
2018-11-02 09:41:26 +0100

    fix for vim color scheme

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,8 @@ find ${MYDIR} -type f -name 'dot.*' | while read DOTFILE; do
     [ ${FORCE} -eq 1 ] && CMD="-sf" || CMD="-s"
     eval ln ${CMD} ${DOTFILE} ${TARGET}
 done
+
+# fix for vim color scheme that is multiple levels deep
+rm ~/.vim__colors__dracula.vim
+mkdir -p ~/.vim/colors
+ln -s ../../dotfiles/vim/dot.vim__colors__dracula.vim .vim/colors/dracula.vim
